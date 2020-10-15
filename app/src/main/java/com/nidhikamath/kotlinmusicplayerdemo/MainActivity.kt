d2c5ff41dfcf32d864a4988b939bb8bd3ce9d721
nidhikamath <kamathnidhi98@gmail.com>
2020-10-15 12:20:34 +0530

    music player play, shuffle

--- app/src/main/java/com/nidhikamath/kotlinmusicplayerdemo/MainActivity.kt
+++ app/src/main/java/com/nidhikamath/kotlinmusicplayerdemo/MainActivity.kt
@@ -2,11 +2,11 @@ package com.nidhikamath.kotlinmusicplayerdemo
 
 import android.Manifest
 import android.content.pm.PackageManager
+import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.Gravity
-import android.view.View
 import android.widget.ImageButton
 import android.widget.ImageView
 import android.widget.TextView
@@ -14,14 +14,13 @@ import android.widget.Toast
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.nidhikamath.kotlinmusicplayerdemo.musicjava.FindMusic
+import com.nidhikamath.kotlinmusicplayerdemo.musicjava.Music
+import java.util.*
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-    private var image: ImageView? = null
-    private var songName: TextView? = null
-    private var artist: TextView? = null
-    private var play: ImageButton? = null
-    private var shuffle: ImageButton? = null
-    val PERMISSION_REQUEST_CODE: Int = 21
+    private val PERMISSION_REQUEST_CODE: Int = 21
+    private var  mediaPlayer: MediaPlayer?=null
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -45,11 +44,46 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun initPlayer(){
-        val musicList = FindMusic(contentResolver)
-        musicList.prepare()
-        musicList.allSongs
-        Log.d("TAG", "songs " + musicList.allSongs.size + " " )
+        val music = FindMusic(contentResolver)
+        music.prepare()
+        val musicList = music.allSongs
+        Log.d("TAG", "songs " + musicList.size + " " )
 
+        playRandom(musicList)
+        shuffle?.setOnClickListener{
+            playRandom(musicList)
+        }
+        play?.setOnClickListener {
+            playOrPause()
+        }
+    }
+
+    private fun playOrPause() {
+        val isPlaying = mediaPlayer?.isPlaying
+        if(isPlaying!!){
+            mediaPlayer?.pause()
+            play?.setImageResource(R.drawable.play)
+        }else{
+            mediaPlayer?.start()
+            play?.setImageResource(R.drawable.pause)
+        }
+
+    }
+
+    private fun playRandom(musicList: MutableList<Music>) {
+        Collections.shuffle(musicList)
+        val music = musicList[0]
+        mediaPlayer?.reset()
+        mediaPlayer = MediaPlayer.create(this, music.uri)
+        mediaPlayer?.setOnCompletionListener {
+            playRandom(musicList)
+        }
+        image?.setImageURI(music.albumArt)
+        songName?.text = music.title
+        artist?.text = music.artist
+
+        mediaPlayer?.start()
+        play?.setImageResource(R.drawable.pause)
 
     }
 
@@ -62,13 +96,18 @@ class MainActivity : AppCompatActivity() {
 
         if (requestCode == PERMISSION_REQUEST_CODE) {
             if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                initPlayer();
+                initPlayer()
             } else {
                 val myToast = Toast.makeText(applicationContext,resources.getString(R.string.acceptpermissions),Toast.LENGTH_SHORT)
-                myToast.setGravity(Gravity.LEFT,200,200)
+                myToast.setGravity(Gravity.START,200,200)
                 myToast.show()
                 finish()
             }
         }
+    }
+
+    override fun onDestroy() {
+        mediaPlayer?.release()
+        super.onDestroy()
     }
 }
